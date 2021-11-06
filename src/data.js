34af860dbd404766c5b03f842439b242594a3472
aarondebarron <aoahorizon@gmail.com>
2021-11-06 00:48:42 +0000

    added frontend part of the react

---
+++ src/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Slim Suit',
+            category: 'wears',
+            image: '/images/product-1.jpg',
+            price: 35000,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+
+        {
+            _id: '2',
+            name: 'Cammon HD Camera',
+            category: 'accesories',
+            image: '/images/product-2.jpg',
+            price: 200000,
+            brand: 'Nike-accesories',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '3',
+            name: 'Nice Snickers',
+            category: 'wears',
+            image: '/images/product-3.jpg',
+            price: 7000,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '4',
+            name: 'Rolex Watch',
+            category: 'wears',
+            image: '/images/product-4.jpg',
+            price: 65000,
+            brand: 'Nike',
+            rating:4.0,
+            numReviewes: 20,
+            description: 'high quaity product'
+            
+        },
+        {
+            _id: '5',
+            name: 'Interrior Decoration',
+            category: 'homes',
+            image: '/images/product-5.jpg',
+            price: 950000,
+            brand: 'Nike-homes',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+        {
+            _id: '6',
+            name: 'Inner Room Bulb',
+            category: 'homes',
+            image: '/images/product-6.jpg',
+            price: 2200,
+            brand: 'Nike-homes',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+            
+        },
+    ]
+}
+
+export default data;
